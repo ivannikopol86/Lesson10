905158a4f66e90586a0d8ad8524aebf7a4738347
Ivan Petrov <ivannikopol86@gmail.com>
2023-03-13 11:59:40 +0300

    DZ

---
+++ src/Lesson10/Exercise1/CallerID.java
@@ -0,0 +1,29 @@
+package Lesson10.Exercise1;
+
+import java.io.*;
+
+
+public class CallerID {
+    public static void printNumbers() {
+
+    File file = new File("C:\\Users\\Pc\\IdeaProjects\\Lesson 10\\src\\Lesson10\\phoneNumbers.txt");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.replaceAll("\\s", "");
+                if (line.length() > 10) {
+                    System.out.println(line);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("No file " + e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+
+}
